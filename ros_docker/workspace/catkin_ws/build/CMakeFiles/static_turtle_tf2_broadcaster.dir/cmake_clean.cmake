4cfd8f4e1a890a1350bddc9e30407763be219c63
johnzaki3 <johnemad96@hotmail.com>
2023-06-02 22:54:33 +0300

    adding exception in .gitignore for /ros_docker/

---
+++ ros_docker/workspace/catkin_ws/build/CMakeFiles/static_turtle_tf2_broadcaster.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/static_turtle_tf2_broadcaster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ros_docker/workspace/catkin_ws/build/CMakeFiles/static_turtle_tf2_broadcaster.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for static_turtle_tf2_broadcaster.
+# This may be replaced when dependencies are built.
