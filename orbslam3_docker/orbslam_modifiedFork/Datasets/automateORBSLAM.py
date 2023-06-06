34d4976343a1295995ace743694f580b96ac7a69
johnzaki3 <johnemad96@hotmail.com>
2023-06-06 09:23:27 +0300

    working on automation of ORBSlam

--- orbslam3_docker/orbslam_modifiedFork/Datasets/automateORBSLAM.py
+++ orbslam3_docker/orbslam_modifiedFork/Datasets/automateORBSLAM.py
@@ -2,24 +2,69 @@
 import subprocess
 import time
 import signal
+import os
 
-# Start a command
-orbslam_process = subprocess.Popen(["rosrun", "ORB_SLAM3" ,"Stereo", "../../ORB_SLAM3/Vocabulary/ORBvoc.txt" ,"../../ORB_SLAM3/Examples/Stereo/EuRoC.yaml", "false"])
+from changeYamlFileValues import Change_Yaml_Parameters
 
-time.sleep(15)
+root_dir = '/Datasets'
 
-startDataset_process = subprocess.Popen(["/Datasets/sendDatasetDoneFlag.sh", "carlaDatasets/38_PaperDataset_Town10_20230331_2_normal_night_euroc.bag"])
+def list_bag_files(directory , filter_bag_files_strings = None):
+    # Initialize an empty list to store the file names
+    bag_files = []
 
-# Wait for a certain amount of time
-time.sleep(90)
+    # Use os.listdir to get the list of all files and directories in the directory
+    for filename in os.listdir(directory):
+        # Check if the file has a .bag extension
+        if filename.endswith('.bag'):
+            # Add the file to the list
+            bag_files.append(os.path.join(directory, filename))
 
-# # If the process is still running after this time, terminate it
-# if process.poll() is None:
-#     process.terminate()
+    # Sort the list of files
+    bag_files.sort()
 
-# If the process is still running after this time, send a SIGINT signal
-if orbslam_process.poll() is None:
-    startDataset_process.send_signal(signal.SIGINT)
-    orbslam_process.send_signal(signal.SIGINT)
-    time.sleep(5)
-    orbslam_process.send_signal(signal.SIGINT)
+    # Initialize an empty list to store the filtered files
+    filtered_files = []
+
+    if filter_bag_files_strings!= None:
+        # Search for the strings in the list of files
+        for file in bag_files:
+            if any(s in file for s in filter_bag_files_strings):
+                filtered_files.append(file)
+    return bag_files, filtered_files
+
+## MISSING:
+#  how to save the output of this in a specific output directory
+def Run_ORBSlam_and_Dataset(rosbagName, testResultDirectory=None):
+    # Start a command
+    # this needs to be run from inside the docker container that has orbslam
+
+    orbslam_process = subprocess.Popen(["rosrun", "ORB_SLAM3" ,"Stereo", "../../ORB_SLAM3/Vocabulary/ORBvoc.txt" ,"../../ORB_SLAM3/Examples/Stereo/EuRoC.yaml", "false"], cwd= )
+
+    time.sleep(15)
+    rosbagName = "carlaDatasets/" + rosbagName
+    startDataset_process = subprocess.Popen(["/Datasets/sendDatasetDoneFlag.sh", rosbagName])
+
+    # Wait for a certain amount of time
+    time.sleep(90)
+
+    # # If the process is still running after this time, terminate it
+    # if process.poll() is None:
+    #     process.terminate()
+
+    # If the process is still running after this time, send a SIGINT signal
+    if orbslam_process.poll() is None:
+        startDataset_process.send_signal(signal.SIGINT)
+        orbslam_process.send_signal(signal.SIGINT)
+        time.sleep(5)
+        orbslam_process.send_signal(signal.SIGINT)
+
+if __name__ == "__main__":
+    # assuming a case where I need to make multiple tests by variyng parameters 
+
+    # list bag files in Datasets/carlaDatasets
+    bag_files, filtered_files = list_bag_files((root_dir +"carlaDatasets/"), ["20230331_2", "normal_night"])
+    
+    # change EuRoC.yaml Parameters, and later this will be a loop to change try multiple parameters and then run the required dataset 
+    Change_Yaml_Parameters(new_ThDepth = 35, new_ORBextractor_nFeatures = 1200)
+    pathToSaveTestResults = "/Datasets/carlaDatasets/"
+    Run_ORBSlam_and_Dataset("38_PaperDataset_Town10_20230331_2_normal_night_euroc.bag", pathToSaveTestResults)
--- orbslam3_docker/orbslam_modifiedFork/Datasets/changeYamlFileValues.py
+++ orbslam3_docker/orbslam_modifiedFork/Datasets/changeYamlFileValues.py
@@ -1,33 +1,41 @@
 #!/usr/bin/env python3
+import os
 # Open the YAML file and read its lines into a list
 
 # update Baseline
-new_ThDepth = 35
+# new_ThDepth = 35
 
 # update number of orb features extracted per frame
-new_ORBextractor_nFeatures = 1200
+# new_ORBextractor_nFeatures = 1200
 
+# directory for yaml file
+root_dir_yaml = "/ORB_SLAM3/Examples"
 
-with open('file.yaml', 'r') as file:
-    lines = file.readlines()
+def Change_Yaml_Parameters(new_ThDepth = -1,new_ORBextractor_nFeatures = -1,Sensor_Setup = "Stereo" ):
+    path_to_yaml = os.path.join(root_dir_yaml, "Stereo" if Sensor_Setup == "Stereo" else "Stereo-Interial")
+    with open(os.path.join(path_to_yaml,'EuRoC.yaml'), 'r') as file:
+        lines = file.readlines()
 
-# Loop over the lines in the file
-for i, line in enumerate(lines):
-    # If this line contains the key you want to change, replace it
-    if ('ThDepth:' in line) and ('new_ThDepth' in globals()):
-        # Split the line into a list of words
-        words = line.split()
-        # Change the value of the key
-        words[1] = str(new_ThDepth) #'new_value_for_ThDepth'
-        # Join the words back into a line and add it back to the list of lines
-        lines[i] = ' '.join(words) + '\n'
-        continue
-    if ('ORBextractor.nFeatures:' in line) and ('new_ORBextractor_nFeatures' in globals()):
-        words = line.split()
-        words[1] = str(new_ORBextractor_nFeatures) #'new_value_for_ORBextractor.nFeatures'
-        lines[i] = ' '.join(words) + '\n'
-        continue
+    # Loop over the lines in the file
+    for i, line in enumerate(lines):
+        # If this line contains the key you want to change, replace it
+        if ('ThDepth:' in line) and (new_ThDepth != -1):
+            # Split the line into a list of words
+            words = line.split()
+            # Change the value of the key
+            words[1] = str(new_ThDepth) #'new_value_for_ThDepth'
+            # Join the words back into a line and add it back to the list of lines
+            lines[i] = ' '.join(words) + '\n'
+            continue
+        if ('ORBextractor.nFeatures:' in line) and (new_ORBextractor_nFeatures != -1):
+            words = line.split()
+            words[1] = str(new_ORBextractor_nFeatures) #'new_value_for_ORBextractor.nFeatures'
+            lines[i] = ' '.join(words) + '\n'
+            continue
 
-# Write the lines back to the YAML file
-with open('file.yaml', 'w') as file:
-    file.writelines(lines)
+    # Write the lines back to the YAML file
+    with open(os.path.join(path_to_yaml,'EuRoC.yaml'), 'w') as file:
+        file.writelines(lines)
+
+if __name__ == "__main__":
+    Change_Yaml_Parameters(new_ThDepth = 35, new_ORBextractor_nFeatures = 1200)
