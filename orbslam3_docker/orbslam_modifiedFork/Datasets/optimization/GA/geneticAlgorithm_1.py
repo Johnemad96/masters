b25e5aaaefaefd3a6afc438825715f5dce1290d1
johnzaki3 <johnemad96@hotmail.com>
2023-07-08 22:00:59 +0300

    Optimization, GA

---
+++ orbslam3_docker/orbslam_modifiedFork/Datasets/optimization/GA/geneticAlgorithm_1.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python3
+import random
+import pandas as pd
+import sys
+import math
+# Parameters
+# The number of individuals in the population.
+POPULATION_SIZE = 70
+
+# The length of the binary string that represents the parameters being optimized.
+# This should be twice the number of bits needed to represent the maximum parameter value in binary,
+# because the binary string is split into two parts to represent two parameters.
+
+#       The range of param1 is 110 (130 - 20) and 
+#       the range of param2 is 900 (1500 - 600), 
+#       so you need at least ceil(log2(110)) = 7 bits for param1 and
+#       ceil(log2(900)) = 10 bits for param2. 
+#       Therefore, the total GENOME_LENGTH should be at least 17 (7 + 10).
+GENOME_LENGTH = 18
+
+# The probability that a bit in a chromosome will be flipped 
+# (changed from 0 to 1 or from 1 to 0) during mutation.
+MUTATION_RATE = 0.1
+# The probability that two parents will be selected for crossover to create a child.
+# This is not used in the provided code, but you could use it in the reproduction method
+# to decide whether to perform crossover.
+CROSSOVER_RATE = 0.8
+
+# The number of generations to run the genetic algorithm.
+# A generation is one cycle of the genetic algorithm, including selection, crossover, mutation, 
+# and fitness evaluation.
+MAX_GENERATIONS = 50
+
+# The number of individuals selected for the tournament in tournament selection.
+# The individual with the highest fitness in the tournament is selected as a parent.
+TOURNAMENT_SIZE = 4
+
+# The number of the fittest individuals that are automatically passed to the next generation.
+# This is a form of elitism, which ensures that the best solutions found so far are not lost.
+ELITISM_SIZE = 5
+
+# Whether to use mutation in the genetic algorithm.
+# If this is set to False, the bitFlipMutation method will not be called, so no bits will be flipped in the chromosomes.
+USE_MUTATION = False
+
+# The method used to select parents for crossover.
+# If this is set to 'tournament', tournament selection is used.
+# If this is set to 'roulette', roulette wheel selection is used.
+SELECTION_METHOD = 'tournament'  # 'tournament' or 'roulette'
+
+# to detect stall generation
+USE_STALL_GEN = False
+STALL_LIMIT = 20
+
+# Initialize stall counter
+stall_counter = 0
+prev_best_fitness = None
+
+#Param1 = baseline
+PARAM1_MIN = 20  # minimum value for parameter 1
+PARAM1_MAX = 120  # maximum value for parameter 1
+GENE1_LENGTH_BINARY = 7 #(UP TO 128)
+
+#Param2 = n of orb features
+PARAM2_MIN = 600  # minimum value for parameter 2
+PARAM2_MAX = 1500 # maximum value for parameter 2
+GENE2_LENGTH_BINARY = 11 #(UP TO 2048)
+
+results = pd.DataFrame(columns=['Generation', 'Solution', 'Fitness', 'Parameters'])
+# PATH_TO_CSV = 
+
+class Chromosome :
+    def __init__(self , length, function):
+        self.genes = ""
+        self.function = function
+        # for i in range(length):
+        #     self.genes += str(random.randint(0, 1)) 
+        self.genes = self.generate_genes(GENE1_LENGTH_BINARY) + self.generate_genes(GENE2_LENGTH_BINARY)
+        self.calculateTheFitness()    
+    
+    def generate_genes(self, length):
+        genes = ""
+        for i in range(length):
+            genes += str(random.randint(0, 1))
+        return genes.zfill(length)
+
+    def calculateTheFitness(self):
+        param1, param2 = self.convertToDecimal()
+        fitnessValue = self.function(param1, param2)
+        self.fitness = fitnessValue
+        
+    def convertToDecimal(self):
+        # Split genes into two parts
+        # genes1 = self.genes[:len(self.genes)//2]
+        # genes2 = self.genes[len(self.genes)//2:]
+        
+        # Split genes into two parts based on the lengths of the binary representations for each parameter
+        genes1 = self.genes[:GENE1_LENGTH_BINARY]
+        genes2 = self.genes[GENE1_LENGTH_BINARY:GENE1_LENGTH_BINARY+GENE2_LENGTH_BINARY]
+    
+        # Convert binary strings to integers
+        decimal1 = int(genes1, 2)
+        decimal2 = int(genes2, 2)
+        
+        # Scale integers to parameter ranges
+        param1 = math.ceil((decimal1 / (2**len(genes1) - 1)) * (PARAM1_MAX - PARAM1_MIN) + PARAM1_MIN) * 1.0
+        param2 = math.ceil((decimal2 / (2**len(genes2) - 1)) * (PARAM2_MAX - PARAM2_MIN) + PARAM2_MIN)
+
+        return param1, param2
+
+class Population :
+    def __init__(self , populationSize , chromosomeSize , function , init):
+        self.chromosomes = []
+        if init :
+            self.chromosomes = [Chromosome(chromosomeSize , function) for i in range(populationSize)]
+            self.chromosomes.sort(key = lambda x:x.fitness, reverse=True)
+            self.fittest = self.chromosomes[0]
+    
+    def getNFittestChromosomes(self, n):
+        self.chromosomes.sort(key = lambda x:x.fitness, reverse=True)
+        return self.chromosomes[:n]
+    
+    def findTheFittest(self):
+        self.chromosomes.sort(key = lambda x:x.fitness, reverse=True)
+        self.fittest = self.chromosomes[0]
+    
+    def calculateTheFitnessForAll(self):
+        for chromosome in self.chromosomes:
+            chromosome.calculateTheFitness()
+
+class GeneticAlgorithm : 
+    def __init__(self , populationSize , chromosomeSize , tournamentSize , elitismSize , mutationRate , function):
+        self.populationSize = populationSize
+        self.chromosomeSize = chromosomeSize
+        self.tournamentSize = tournamentSize
+        self.elitismSize    = elitismSize
+        self.mutationRate   = mutationRate
+        self.function       = function
+    
+    def reproduction(self , population):
+        temp = []
+        temp[:self.elitismSize] = population.getNFittestChromosomes(self.elitismSize)
+        for i in range(self.elitismSize , self.populationSize):
+            if SELECTION_METHOD == 'tournament':
+                parent1 = self.tournamentSelection(population)
+                parent2 = self.tournamentSelection(population)
+            elif SELECTION_METHOD == 'roulette':
+                parent1 = self.rouletteWheelSelection(population)
+                parent2 = self.rouletteWheelSelection(population)
+            
+            child = self.onePointCrossOver(parent1, parent2)
+            
+            if USE_MUTATION:
+                self.bitFlipMutation(child)
+            
+            temp.append(child)
+            
+        newPopulation = Population(self.populationSize, self.chromosomeSize, self.function, False)
+        newPopulation.chromosomes = temp
+        newPopulation.findTheFittest()
+        newPopulation.calculateTheFitnessForAll()
+        return newPopulation
+        
+    def bitFlipMutation(self , child):
+        if random.random() < self.mutationRate :
+            mutationPoint = random.randint(0, len(child.genes) -1)
+            geneslist = list(child.genes)
+            geneslist[mutationPoint] = "0" if geneslist[mutationPoint] == "1" else "1"
+            child.genes = ''.join(geneslist)
+            child.calculateTheFitness()
+
+    def tournamentSelection(self , population):
+        tournamentPool = []
+        for i in range(self.tournamentSize):
+            index = random.randint(0, len(population.chromosomes) -1)
+            tournamentPool.append(population.chromosomes[index])
+        tournamentPool.sort(key = lambda x:x.fitness, reverse=True)
+        return tournamentPool[0]
+
+    def rouletteWheelSelection(self, population):
+        total_fitness = sum(chromosome.fitness for chromosome in population.chromosomes)
+        pick = random.uniform(0, total_fitness)
+        current = 0
+        for chromosome in population.chromosomes:
+            current += chromosome.fitness
+            if current > pick:
+                return chromosome
+    
+    def onePointCrossOver(self , parent1 , parent2):
+        temp = []
+        crossOverPoint = random.randint(0, len(parent1.genes) -1)            
+        temp[:crossOverPoint] = parent1.genes[:crossOverPoint]
+        temp[crossOverPoint:] = parent2.genes[crossOverPoint:]
+        child = Chromosome(self.chromosomeSize, self.function)
+        child.genes = ''.join(temp)
+        child.calculateTheFitness()
+        return child
+    
+# Function to be optimized
+def f(param1, param2):
+    print("#$%*** PARAMERTERS = ",param1,param2)
+    sys.exit()
+    # Here, replace with your SLAM system function
+    # Run your SLAM system with param1 and param2, get the trajectory and calculate the RMSE
+
+    # return the negative RMSE as the fitness
+    # return -RMSE
+    pass
+
+# Generate the initial population
+initialPopulation = Population(populationSize = POPULATION_SIZE, chromosomeSize = GENOME_LENGTH, function = f, init = True)
+
+# Create an instance of Genetic Algorithm
+GeneticAlgo = GeneticAlgorithm(populationSize = POPULATION_SIZE, chromosomeSize = GENOME_LENGTH, tournamentSize = TOURNAMENT_SIZE, elitismSize = ELITISM_SIZE, mutationRate = MUTATION_RATE, function = f)
+
+population = initialPopulation
+
+# Repeat the process for the number of generations
+for i in range(MAX_GENERATIONS):    
+    population = GeneticAlgo.reproduction(population)
+    param1, param2 = population.fittest.convertToDecimal()
+    print("# Generation: ", i)
+    print("\t solution: ", population.fittest.genes)
+    print("\t fitness: ", population.fittest.fitness)
+    print("\t parameters: ", param1, param2)
+    results = results.append({
+    'Generation': i,
+    'Solution': population.fittest.genes,
+    'Fitness': population.fittest.fitness,
+    'Parameters': (param1, param2)
+    }, ignore_index=True)
+    # Check if fitness has improved
+    if USE_STALL_GEN != True:
+        continue
+    if prev_best_fitness is not None and population.fittest.fitness <= prev_best_fitness:
+        stall_counter += 1
+    else:
+        stall_counter = 0
+    
+    # Update previous best fitness
+    prev_best_fitness = population.fittest.fitness
+    
+    # Check if stall limit has been reached
+    if stall_counter >= STALL_LIMIT:
+        print("Terminating due to stall in fitness improvement.")
+        break
+
+# Print the best solution
+print("Best solution: ", population.fittest.genes)
+print("Best fitness: ", population.fittest.fitness)
+best_param1, best_param2 = population.fittest.convertToDecimal()
+print("Best parameters: ", best_param1, best_param2)
+results.to_csv('results.csv', index=False)
---
+++ orbslam3_docker/orbslam_modifiedFork/Datasets/optimization/GA/test.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+import os
+import sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# parent_dir = os.path.dirname(current_dir)
+parent_dir = os.sep.join(current_dir.split(os.sep)[:-2])
+print(parent_dir)
+sys.path.insert(1, parent_dir)
+
+import rospy
+
+from changeYamlFileValues import Change_Yaml_Parameters
+from readParams import Read_Required_Params
+from createIcrementedDatasetTestFolder import create_incremented_folder
+from parseResults import ParseResults_SubSubfolder, Update_Global_Variables
+from automateORBSLAM import list_bag_files, Run_ORBSlam_and_Dataset
+from parseResults_Generic import find_files
+from std_msgs.msg import String, Float32
+# quick test
+# import sys
+# # import rospy
+# from learning_tf2.srv import *
+
+received_rmse = 0
+last_received_rmse = 0
+
+def is_float_num(n):
+    return isinstance(n, (int, float)), isinstance(n, float)
+
+class Receiver:
+    def __init__(self,rospy):
+        self.received_expected_data = False
+        # rospy.init_node('receiver')
+        rospy.Subscriber('rmse_to_GA', Float32, self.callback)
+
+    def callback(self, msg):
+        global received_rmse
+        rospy.loginfo('Received: %s', msg.data)
+        is_digit, is_float = is_float_num(msg.data)
+        if is_digit == True:
+            self.received_expected_data = True
+            received_rmse = msg.data
+
+    def spin(self):
+        rate = rospy.Rate(1)  # 1 Hz
+        while not rospy.is_shutdown() and not self.received_expected_data:
+            rate.sleep()
+        self.received_expected_data = False  
+
+class Sender:
+    def __init__(self,rospy):
+        # rospy.init_node('sender')
+        self.publisher = rospy.Publisher('cmd_from_GA', String, queue_size=10)
+    def send(self, data):
+        msg = Float32()
+        msg.data = data
+        self.publisher.publish(msg)
+
+
+if __name__ == "__main__":
+
+    parameters = Read_Required_Params('optimizationParameters.txt')
+    dataset_dir = parameters['dataset_dir']
+    root_dir = parameters['root_dir']
+    test_results_path = parameters['test_results_path']
+    test_parameter = parameters['test_parameter']
+
+    bag_files, filtered_files = list_bag_files(dataset_dir, ["20230331_1", "normal"])
+    pathToSaveTestResults_testParameter = create_incremented_folder(test_results_path, folder_name_suffix=test_parameter)
+
+    new_ThDepth = 95.0
+    new_ORBextractor_nFeatures = 600
+
+    new_ThDepth = new_ThDepth*1.0
+    Change_Yaml_Parameters(new_ThDepth=new_ThDepth, new_ORBextractor_nFeatures = new_ORBextractor_nFeatures)
+    # pathToSaveTestResults = os.path.join(pathToSaveTestResults_testParameter,((str(int(new_ThDepth)).zfill(4)) +"_"+ (str(new_ORBextractor_nFeatures).zfill(4))).replace('.', '_'))
+
+    pathToSaveTestResults = create_incremented_folder(pathToSaveTestResults_testParameter, folder_name_suffix=((str(int(new_ThDepth)).zfill(4)) +"_"+ (str(new_ORBextractor_nFeatures).zfill(4))).replace('.', '_'))
+
+    # os.makedirs(pathToSaveTestResults, exist_ok=True)
+    print("running orb slam")
+    Run_ORBSlam_and_Dataset(filtered_files[0], pathToSaveTestResults)
+    # time.sleep(3)
+
+    # full_path, relative_path = find_files(parameters['ROOT_DIR'], "FrameTrajectory_TUM_Format.txt", parameters['STEREO_FOLDER_NAME'],parameters['test_parameter'])
+    evaluationParameters =Update_Global_Variables("parseResultsParameters.txt")
+    _ , resultInstance_Index = os.path.split(pathToSaveTestResults_testParameter)
+    subfolder_path, subfolder = os.path.split(pathToSaveTestResults)
+    subfolder_path = (os.path.join(subfolder_path, subfolder))
+    print(resultInstance_Index,"\n", subfolder,"\n" ,subfolder_path,"\n", os.getcwd())
+    if "FrameTrajectory_TUM_Format.txt" in sorted(os.listdir(pathToSaveTestResults)):
+        eval_command = ParseResults_SubSubfolder(subfolder, sorted(os.listdir(subfolder_path)),
+                                subfolder_path, 
+                                dataset_ground_truth_folder=evaluationParameters['dataset_ground_truth_folder'], 
+                                Stereo_Folder_Name=resultInstance_Index, 
+                                external_server_evaluation=True)
+        print(eval_command)
+    # eval_command = "evo_ape tum /Datasets/optimization/GA/Daytime_Normal_GroundTruth_Transformed_clean.tum /Datasets/optimization/GA/GAtests/21_2params_baseline_ORBextractor_nFeatures/01_0095_0600/FrameTrajectory_TUM_Format.txt --align --save_results /Datasets/optimization/GA/GAtests/21_2params_baseline_ORBextractor_nFeatures/01_0095_0600/21_2params_baseline_ORBextractor_nFeatures_01_0095_0600_ALIGN_results.zip"
+    # rospy.wait_for_service('evaluate_SLAM_Evo')
+    # try:
+    #     evaluate_SLAM_Evo = rospy.ServiceProxy('evaluate_SLAM_Evo', evaluateSLAMEvo)
+    #     resp1 = evaluate_SLAM_Evo(eval_command)
+    # except rospy.ServiceException as e:
+    #     print("Service call failed: %s"%e)    
+    rospy.init_node('GA_Node')
+    receiver = Receiver(rospy)
+    sender = Sender(rospy)
+    sender.send(eval_command)
+    receiver.spin()
+    print(received_rmse)
